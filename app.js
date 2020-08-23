5458fa03ae44844dc8d5a49732eb49bb995aca76
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-23 13:53:39 +0200

    created constructor and one dino object

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
+// Create Dino Constructor
+function Dino(species, weight, height, diet, where, when, fact, image) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+  this.image = image;
+}
 
-    // Create Dino Constructor
+// Create Dino Objects
+const triceratops = new Dino(
+  "Triceratops",
+  5897,
+  2.9,
+  "herbavor",
+  "North America",
+  "Late Cretaceous",
+  "First discovered in 1889 by Othniel Charles Marsh",
+  "triceratops.png"
+);
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Dino Objects
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Human Object
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Generate Tiles for each Dino in Array
 
+// Add tiles to DOM
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
