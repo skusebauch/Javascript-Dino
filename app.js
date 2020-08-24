443e740b83c4ec404927a2bfb701a9f9d25ffba6
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-24 23:43:36 +0200

    create all dino objects

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function Dino(species, weight, height, diet, where, when, fact, image) {
 }
 
 // Create Dino Objects
-const triceratops = new Dino(
+let triceratops = new Dino(
   "Triceratops",
   5897,
   2.9,
@@ -21,6 +21,73 @@ const triceratops = new Dino(
   "First discovered in 1889 by Othniel Charles Marsh",
   "triceratops.png"
 );
+
+let tyrannosaurusRex = new Dino(
+  "Tyrannosaurus Rex",
+  5400,
+  3.66,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "The largest known skull measures in at 5 feet long.",
+  "tyrannosaurus rex.png"
+);
+
+let brachiosaurus = new Dino(
+  "Brachiosaurus",
+  3175,
+  9.45,
+  "herbavor",
+  "North America",
+  "Late Jurasic",
+  "An asteroid was named 9954 Brachiosaurus in 1991.",
+  "brachiosaurus.png"
+);
+
+let stegosaurus = new Dino(
+  "Stegosaurus",
+  5262,
+  2.01,
+  "herbavor",
+  "North America, Europe, Asia",
+  "Late Jurasic to Early Cretaceous",
+  "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+  "stegosaurus.png"
+);
+
+let elasmosaurus = new Dino(
+  "Elasmosaurus",
+  7257,
+  1.5,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Elasmosaurus was a marine reptile first discovered in Kansas.",
+  "elasmosaurus.png"
+);
+
+let pteranodon = new Dino(
+  "Pteranodon",
+  20,
+  0.51,
+  "carnivor",
+  "North America",
+  "Late Cretaceous",
+  "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+  "pteranodon.png"
+);
+
+let pigeon = new Dino(
+  "Pigeon",
+  0.23,
+  0.13,
+  "herbavor",
+  "World Wide",
+  "Holocene",
+  "All birds are living dinosaurs",
+  "pigeon.png"
+);
+
 // Create Human Object
 
 // Use IIFE to get human data from form
